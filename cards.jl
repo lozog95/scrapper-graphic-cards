c3851f992d785aa2fa9622f98cd1b4bf17d4b53a
lozog95 <lozog95@gmail.com>
2022-01-10 21:52:28 +0100

    restructured for heroku

---
+++ cards.jl
@@ -0,0 +1,1 @@
+{"price": "4599", "card": "PNY GeForce RTX 3060 Ti 8GB XLR8 Gaming REVEL EPIC-X RGB Dual Fan (LHR) VCG3060T8LDFXPPB", "link": "https://mediamarkt.pl/komputery-i-tablety/karta-graficzna-pny-geforce-rtx-3060-ti-8gb-xlr8-gaming-revel-epic-x-rgb-dual-fan-lhr-vcg3060t8ldfxppb"}
