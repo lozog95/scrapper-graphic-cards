f0f8d805dedc06c82a27932606995b1ab3b69e40
lozog95 <lozog95@gmail.com>
2022-01-10 21:57:02 +0100

    added build files for heroku

--- Scrapper_graphics_card/settings.py
+++ Scrapper_graphics_card/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import posixpath
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,3 +113,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+
+django_heroku.settings(locals())
