fea96c8511b5f4538ba5ff1ff14cee42f128e4fe
lozog95 <lozog95@gmail.com>
2022-01-10 01:18:08 +0100

    updated name

--- src/euro.py
+++ src/euro.py
@@ -2,11 +2,10 @@ import scrapy
 from html import unescape
 import unicodedata
 import time
-
 class EuroGCSpider(scrapy.Spider):
     name = 'cards'
     start_urls = [
-        'https://www.euro.com.pl/karty-graficzne.bhtml',
+        'https://www.euro.com.pl/karty-graficzne,typ-chipsetu!geforce-rtx-3060.bhtml',
     ]
 
     def _cleanup_price(self, price):
@@ -17,19 +16,18 @@ class EuroGCSpider(scrapy.Spider):
         return price
 
     def _cleanup_name(self, name):
-        name=name.strip()
-        name=name.replace("\u00ae","")
+        name = name.lstrip("karta graficzna ")
         return name
 
     def parse(self, response):
         for card in response.css('div.product-for-list'):
             yield {
                 'price': self._cleanup_price(unicodedata.normalize('NFKC', unescape(card.css('div.price-normal::text').get()))),
-                'card': self._cleanup_name(unicodedata.normalize('NFKC', unescape(card.css('span.attribute-value::text').get()))),
+                'card': self._cleanup_name(card.css('a.js-save-keyword > img::attr("alt")').get()),
                 'link': "https://euro.com.pl"+card.css('a.js-save-keyword::attr("href")').get()
             }
         
 
         next_page=response.css('a.paging-number::attr("href")').get()
-        if next_page is not None and next_page != "/karty-graficzne.bhtml":
+        if next_page is not None and next_page != "/karty-graficzne,typ-chipsetu!geforce-rtx-3060.bhtml":
             yield response.follow(next_page, self.parse)
